5233dc47ca579d4dcd218d2b348f3d4a7852fd12
CedricVanRoeyen <cedric.freelancer@gmail.com>
2015-12-06 22:12:37 +0100

    This is a game where I am working on to learn the basics of game development
    This is my first ever game I will be releasing

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var Runner = Runner || {};
+
+var GAME_WIDTH = 512;
+var GAME_HEIGHT = 288;
+
+var game = new Phaser.Game(GAME_WIDTH, GAME_HEIGHT, Phaser.CANVAS, "container");
+
+var Background = new Background(game);
+var MenuScreen = new MenuScreen(game);
+var Player = new Player(game);
+
+game.state.add("boot", BootState);
+game.state.add("preload", PreloadState);
+game.state.add("menu", MenuState);
+game.state.add("game", GameState);
+game.state.start("boot");
